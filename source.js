66d74a588ee0e9bd0b9fdd45489d575db0a1b2f1
Karimullah <59876062+KarimullahOfficial@users.noreply.github.com>
2023-07-18 13:06:51 +0500

    i

--- source.js
+++ source.js
@@ -2,5 +2,24 @@ console.log("hello world");
 // variable constant and comment
 // variable redecaler and reintilization
 var a = 34;
-var a=45
+var a = 45;
 console.log(a);
+console.log(a);
+// we can not redeclare the but we can reassign value
+let b = 4;
+b = 5;
+console.log(b);
+
+// const
+const c = 8;
+
+console.log(c);
+
+//there are 7 datatype
+//int 1,2324, can store data type
+//string  "123", "karimulllah"
+//boolen true/ false
+//null special  value absence
+//Undefine declare but  not initial
+//symbol define 
+//object
